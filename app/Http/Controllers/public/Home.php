8fe8de9558a879db1ac2cb6f6657fb571ee19f78
bertojuni <berto1700018224@webmai.uad.ac.id>
2021-06-17 20:03:38 +0700

    masih eror

---
+++ app/Http/Controllers/public/Home.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+
+class Home extends Controller
+{
+    /**
+     * Show the profile for a given user.
+     *
+     * @param  int  $id
+     * @return \Illuminate\View\View
+     */
+    public function index()
+    {
+        //return view('public/index');
+    }
+}
---
+++ resources/views/public/layout/head.blade.php
@@ -0,0 +1,20 @@
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>@yield('title')</title>
+    <link rel="stylesheet" href="{ assets('public/vendors/mdi/css/materialdesignicons.min.css') }">
+    <link rel="stylesheet" href="{ assets('public/vendors/owl.carousel/css/owl.carousel.css') }">
+    <link rel="stylesheet" href="{ assets('public/vendors/owl.carousel/css/owl.theme.default.min.css') }">
+    <link rel="stylesheet" href="{ assets('public/vendors/aos/css/aos.css') }">
+    <link rel="stylesheet" href="{ assets('public/vendors/jquery-flipster/css/jquery.flipster.css') }">
+    <link rel="stylesheet" href="{ assets('public/css/style.css') }">
+    <link rel="shortcut icon" href="{ ('public\images\favicon.png') }" />
+</head>
+
+@yield('container')
+
+<script src="{ assets('/public/vendors/base/vendor.bundle.base.js') }"></script>
+<script src="{ assets('/public/vendors/owl.carousel/js/owl.carousel.js') }"></script>
+<script src="{ assets('/public/vendors/aos/js/aos.js') }"></script>
+<script src="{ assets('/public/js/template.js') }"></script>
